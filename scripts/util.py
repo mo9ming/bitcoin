1ebfd7caabd39732a616502c3d31293fb4060439
ysyx_220042-Su Hailing <mo9ming@163.com>
2022-04-07 09:27:25 +0800

    first commit

---
+++ scripts/util.py
@@ -0,0 +1,13 @@
+def str2Hexstr(origin):
+	return ''.join(['{:x}'.format(ord(i)) for i in origin])
+
+def hexstr2Str(origin):
+	return ''.join([chr(int(origin[i:i + 2], 16)) for i in range(0, len(origin), 2)])
+
+print(chr(49))
+print(str2Hexstr(chr(49)))
+print(hexstr2Str(str2Hexstr(chr(49))))
+
+print('hello')
+print(str2Hexstr('hello'))
+print(hexstr2Str(str2Hexstr('hello')))
